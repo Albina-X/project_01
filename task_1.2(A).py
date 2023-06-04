a1f57190fd47274e9486cdaecf261b2a75b7848f
albina <ohahao93@gmail.com>
2023-06-05 00:03:54 +0400

    Task 1.2(A,B,C) - completed

---
+++ task_1.2(A).py
@@ -0,0 +1,32 @@
+# Задача 1.2. Пункт A. 
+
+
+# Приведен плейлист песен в виде списка списков, в котором список my_favorite_songs содержит список названий и длительности каждого трека
+# Вывести общее время звучания трех случайных песен в формате "Три песни звучат ХХХ минут"
+
+# Решение:
+# Сначала сгенерируем список трех случайных песен:
+
+import random
+my_favorite_songs = [
+    ['Waste a Moment', 3.03],
+    ['New Salvation', 4.02],
+    ['Staying\' Alive', 3.40],
+    ['Out of Touch', 3.03],
+    ['A Sorta Fairytale', 5.28],
+    ['Easy', 4.15],
+    ['Beautiful Day', 4.04],
+    ['Nowhere to Run', 2.58],
+    ['In This World', 4.02],
+]
+random_songs = random.sample(my_favorite_songs,3)
+
+# Далее сложим длительность выбранных песен и выведем результат в терминал:
+
+sum = 0
+a = []
+for i in random_songs:
+    a.append (i[1])
+    sum = sum + i[1]
+
+print('Три песни:', random_songs[0][0], ',', random_songs[1][0], ',', random_songs[2][0], 'звучат', sum, 'минут.')
---
+++ task_1.2(B).py
@@ -0,0 +1,28 @@
+# Задача 1.2. Пункт B.
+
+# Есть словарь песен. Необходимо распечатать общее время звучания трех случайных песен и вывести: Три песни звучат ХХХ минут.
+
+# Решение:
+
+import random
+
+my_favorite_songs_dict = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+# Сначала сгенерируем список трех случайных песен:
+
+random_songs = random.sample(list(my_favorite_songs_dict.keys()), 3)
+
+# Далее сложим длительность выбранных песен и выведем результат в терминал:
+
+total_time = sum(my_favorite_songs_dict[song] for song in random_songs)
+
+print('Три песни:', random_songs, 'звучат',  total_time, 'минут.')
